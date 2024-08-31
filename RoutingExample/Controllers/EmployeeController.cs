e8e3a096886e8fd8f6fa31c2983e90dc0eb9b528
Surya C Urs <surya.c.urs@intimetec.com>
2024-09-01 02:37:17 +0530

    first commit

---
+++ RoutingExample/Controllers/EmployeeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RoutingExample.Controllers
+{
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        //attribute routing. defining the uri by us only
+        [Route("Emp/All")]
+        [Route("GetAllEmployees")] //can give multiple url for a particular action method using attribute routing
+        [HttpGet]
+        public string GetAllEmployee()
+        {
+            return "Invoking GetAllEmployee method";
+        }
+
+        //attribute routing. defining the uri by us only
+        [Route("Emp/ById/{Id}")]
+        [HttpGet]
+        public string GetEmployeeById(int Id)
+        {
+            return "Invoking GetEmployeeById method";
+        }
+
+        //http verb attribute routing. 
+        [HttpGet("api/Employee/{Name}")]
+        public string GetEmployeeByName()
+        {
+            return "Invoking GetEmployeeByName method";
+        }
+
+        [HttpGet("api/Employee/{Name}")]
+        public string GetEmployeeName()
+        {
+            return "Invoking GetEmployeeByName method";
+        }
+    }
+}
